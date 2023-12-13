576030be49efbe7d029069d549c7a08cfe357899
AishaBakare <bakareaisha23@gmail.com>
2023-12-13 09:57:36 +0100

    contact completed

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,9 @@
 //Where all JavaScript codes would be written
+const d = new Date();
+document.getElementById("demo").innerHTML = d.toDateString();
+
+
+if (new Date().getHours() < 18) {
+    document.getElementById("demo2").innerHTML = "Good day!";
+  }
+ 
